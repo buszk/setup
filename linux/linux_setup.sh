b38ff85704b7cf344e55b19969d4ce3c73f95154
Zekun Shen <bruceshenzk@gmail.com>
2024-10-15 21:33:17 -0500

    Ubuntu 24

--- linux/linux_setup.sh
+++ linux/linux_setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 sudo apt install -y vim gcc make bison build-essential gettext git pkg-config \
-    python ssh subversion wget time curl zsh ctags vim openssh-server flex net-tools
+    ssh subversion wget time curl zsh vim openssh-server flex net-tools
 
 # Organize directories
-mkdir ~/Workspace/
-
-( cd ~/Workspace && \
-  mkdir git )
+mkdir -p ~/Workspace/git
 
 # Install oh-my-zsh and configure it
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
